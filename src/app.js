0866c540e9fa142ec59f10a3b4b96265ca667127
Amitava <amitava.bs@gmail.com>
2014-08-03 14:35:58 +0530

    Add `Load project` menu item
    Uses `invoker start` command to load a project.

--- src/app.js
+++ src/app.js
@@ -4,6 +4,25 @@ var app = require('app');
 var Menu = require('menu');
 var MenuItem = require('menu-item');
 var Tray = require('tray');
+var dialog = require('dialog');
+var exec = require('child_process').exec;
+
+function loadProject() {
+  var files = dialog.showOpenDialog({
+    properties: [ 'openFile' ],
+    title: 'Open Invoker config file'
+  });
+
+  if (Array.isArray(files)) {
+    exec('invoker start ' + files[0] + ' -d', function(error, stdout, stderr) {
+      if (!error) {
+        // TODO: update menu
+      } else {
+        console.log(error);
+      }
+    });
+  }
+}
 
 var tray = null;
 app.on('ready', function() {
@@ -11,6 +30,8 @@ app.on('ready', function() {
   tray.setPressedImage(__dirname + '/tray-icon-alt.png');
 
   var contextMenu = Menu.buildFromTemplate([
+    { label: 'Load project', click: loadProject },
+    { type: 'separator' },
     { label: 'Quit', click: function() { app.quit(); } }
   ]);
   tray.setContextMenu(contextMenu);
